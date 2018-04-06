26d2b421cb2e5a448a69ebdeedd70771bdbf054f
Shubham Singh <coolsks94@gmail.com>
2018-04-06 22:06:59 +0000

    thyme: Move Xiaomi Doze inside stock ambient display preference
    Change-Id: I8193fe58411e491155fa6c68d4a3e7bacc1fcbe5

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    SettingsOverlayThyme
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
