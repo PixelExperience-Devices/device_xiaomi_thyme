473490fc8c42b52aeeb7457dbc6b414fbc98dad8
luca020400 <luca.stefani.ge1@gmail.com>
2020-12-06 17:11:09 +0100

    thyme: Set boot animation resolution
    Change-Id: Ib1a3d2a4ed498199920f196f4f69e03de1128d0d

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
