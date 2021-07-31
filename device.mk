d3c589eba014e16ef26a0a5b512e5ad2474fa342
TH779 <i@779.moe>
2021-07-31 18:34:22 +0800

    thyme: Set shipping API level
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I205540413d7f445c1c3b666c9dc02b5b83900714

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 30
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
