70f6e830695d813a79dcbd05cd57b26342c655b3
TH779 <i@779.moe>
2021-08-14 23:07:31 +0800

    thyme: Import WifiResCommon
     * From V12.5.2.0.RJKCNXM.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I3b01d84b37a243568fbf86f7d57a391f77d166c6

--- device.mk
+++ device.mk
@@ -221,6 +221,9 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # WiFi
+PRODUCT_PACKAGES += \
+    WifiResCommon
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6390/WCNSS_qcom_cfg.ini \
