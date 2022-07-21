a6f493fb062c85145c7c2e344732be3cede60918
LynnrinChan <wuxiaofei31@gmail.com>
2022-07-21 16:35:39 +0800

    thyme: Import QTI whitelist XML
     * From LA.UM.9.12.r1-14300-SMxx50.0.
    
    Signed-off-by: LynnrinChan <wuxiaofei31@gmail.com>
    Change-Id: Idce64c330d537285397dc6bf6973bf145d259428

--- device.mk
+++ device.mk
@@ -143,6 +143,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
