d5dd62ed89b75c8c12f111cdd0450af9db3deac8
LynnrinChan <wuxiaofei31@gmail.com>
2022-09-03 23:42:49 +0800

    thyme: Update USB HAL to 1.3

--- device.mk
+++ device.mk
@@ -462,7 +462,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-qti
+    android.hardware.usb@1.3-service-qti
 
 # Vendor boot modules
 PRODUCT_COPY_FILES += \
