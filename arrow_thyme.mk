5ae74f819322fc362f22577d4d2b91c3616708a5
LynnrinChan <lynnrin@lynnrin.moe>
2022-09-15 10:43:11 +0800

    thyme: Update blobs from V13.0.8.0.RJKCNXM

--- arrow_thyme.mk
+++ arrow_thyme.mk
@@ -25,4 +25,4 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/thyme/thyme:12/SKQ1.211006.001/V13.0.3.0.SGACNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/thyme/thyme:12/SKQ1.211006.001/V13.0.8.0.SGACNXM:user/release-keys
